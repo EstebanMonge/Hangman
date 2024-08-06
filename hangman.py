9328e7af0a63fee7e6ef1e0bd5de37d0c5ee5e69
Esteban Monge <em1069@angellodebiansofia>
2024-08-06 15:19:36 -0600

    Disable used letter, draw every time new game the board

--- hangman.py
+++ hangman.py
@@ -17,6 +17,17 @@ def juegoNuevo():
     # Asigna a la etiqueta la palabra reemplazando letras con guiones bajos
     etiquetaPalabra.set(' '.join("_"*len(la_palabra)))
     imgEtiqueta.config(image="")
+    #Crea la butonera con todos las letras
+    #Cada vez que se oprime una letra llama a la función adivinar
+    n=0
+    for c in ascii_uppercase:
+       try:
+          ventana.nametowidget("."+c.lower()).destroy()
+       except:
+          print("Already deleted")
+       Button(ventana, name=c.lower(), text=c, command=lambda c=c: adivinar(c), font=('Helvetica 18'), width=4).grid(row=1+n//9,column=n%9)
+       n+=1
+
 
 # Funcion que tiene la logica para adivinar
 def adivinar(letra):
@@ -24,20 +35,37 @@ def adivinar(letra):
         global numeroDeIntentos
         # Si es menor a 11 otorga un intento
         if numeroDeIntentos<11:
+                # Crea una lista con la palabra
                 texto = list(la_palabra_conEspacios)
+                # Crea una lista con los espacios de la palabra y las letras adivinadas
                 adivinado = list(etiquetaPalabra.get())
+                # Cuenta cuantas veces aparece la letra en la palabra, si mayor que 0
                 if la_palabra_conEspacios.count(letra)>0:
+                        # Inicia el ciclo para reemplazar la letra adivinada
                         for c in range(len(texto)):
+                                # Si letra del texto igual a letra adivinada
                                 if texto[c]==letra:
+                                        # Reemplaza guion por letra
                                         adivinado[c]=letra
-                                        ventana.nametowidget("."+letra.lower()).destroy()
+                                        # Deshabilita el boton con la letra
+                                        try:
+                                           ventana.nametowidget("."+letra.lower()).destroy()
+                                        except:
+                                           print("Already deleted")
+                                # Reemplaza en la etiqueta las letras adivinadas
                                 etiquetaPalabra.set("".join(adivinado))
+                                # Si la etiqueta es igual a la palabra con espacios
                                 if etiquetaPalabra.get()==la_palabra_conEspacios:
+                                        # Emite que ganó el luego
                                         messagebox.showinfo("Ahoracado","Ganaste! =)")
                                         break
+                # Si menor que cero
                 else:
+                        # Aumenta el número de intentos
                         numeroDeIntentos += 1
+                        # Obtiene una imagen del arreglo de imagenes
                         imgEtiqueta.config(image=fotos[numeroDeIntentos])
+                        # Si la cantidad de intentos es igual a 11... indica que perdió
                         if numeroDeIntentos==11:
                                         messagebox.showwarning("Ahocado","Perdiste =(")
 
@@ -63,13 +91,6 @@ imgEtiqueta.grid(row=0, column=0, columnspan=3, padx=10, pady=40)
 #Se crea el espacio donde se dibuja los espacios con las letras de la palabra a adivinar
 etiquetaPalabra = StringVar()
 Label(ventana, textvariable  =etiquetaPalabra,font=('consolas 24 bold')).grid(row=0, column=3 ,columnspan=6,padx=10)
-
-#Crea la butonera con todos las letras
-#Cada vez que se oprime una letra llama a la función adivinar
-n=0
-for c in ascii_uppercase:
-    Button(ventana, name=c.lower(), text=c, command=lambda c=c: adivinar(c), font=('Helvetica 18'), width=4).grid(row=1+n//9,column=n%9)
-    n+=1
 
 #Crea el botón de juego nuevo
 Button(ventana, text="Juego\nnuevo", command=lambda:juegoNuevo(), font=("Helvetica 10 bold")).grid(row=3, column=8)
